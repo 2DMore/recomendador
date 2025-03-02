f3dcaadff504beb9d795977a717717d58d7f0efa
David Pat <picarofin@gmail.com>
2025-03-02 16:00:18 -0600

    Cambios porque el navbar no tiene -1

--- app/Helpers/NavBarHelper.php
+++ app/Helpers/NavBarHelper.php
@@ -1,71 +1,87 @@
 <?php
-function get_nav_elements($option) {
+function get_nav_elements($option)
+{
 	$index = $option - 1;
 	$navbar_options = [
-		0=>[//Administrador
+		0 => [ //Administrador
 			[
-				'link'=>'./listar',
-				'img_source'=>'images/document.svg',
+				'link' => './listar',
+				'img_source' => 'images/document.svg',
 				'text' => 'Listado de documentos',
 			],
 			[
-				'link'=>'./usuarios',
-				'img_source'=>'images/users.svg',
+				'link' => './usuarios',
+				'img_source' => 'images/users.svg',
 				'text' => 'Gestionar usuarios',
 			],
 			[
-				'link'=>'./agregar-usuarios',
-				'img_source'=>'images/user.svg',
+				'link' => './agregar-usuarios',
+				'img_source' => 'images/user.svg',
 				'text' => 'Agregar usuarios',
 			],
 			[
-				'link'=>'./estadisticas',
-				'img_source'=>'images/graph.svg',
+				'link' => './estadisticas',
+				'img_source' => 'images/graph.svg',
 				'text' => 'Estadísticas',
 			]
 		],
-		1=>[//Usuario
+		1 => [ //Usuario
 			[
-				'link'=>'./nuevos',
-				'img_source'=>'images/edit.svg',
+				'link' => './nuevos',
+				'img_source' => 'images/edit.svg',
 				'text' => 'Documentos nuevos',
 			],
 			[
-				'link'=>'./validados',
-				'img_source'=>'images/files.svg',
+				'link' => './validados',
+				'img_source' => 'images/files.svg',
 				'text' => 'Documentos validados',
 			],
 			[
-				'link'=>'./estadisticas',
-				'img_source'=>'images/graph.svg',
+				'link' => './estadisticas',
+				'img_source' => 'images/graph.svg',
 				'text' => 'Estadísticas',
 			]
 		],
-		2=>[//Validadores
+		2 => [ //Validadores
 			[
-				'link'=>'./newlist',
-				'img_source'=>'images/edit.svg',
+				'link' => './newlist',
+				'img_source' => 'images/edit.svg',
 				'text' => 'Documentos registrados',
 			],
 			[
-				'link'=>'./capturados',
-				'img_source'=>'images/files.svg',
+				'link' => './capturados',
+				'img_source' => 'images/files.svg',
 				'text' => 'Documentos capturados',
 			],
 
 			[
-				'link'=>'./validados',
-				'img_source'=>'images/valid.svg',
+				'link' => './validados',
+				'img_source' => 'images/valid.svg',
 				'text' => 'Validados',
 			],
 			[
-				'link'=>'./estadisticas',
-				'img_source'=>'images/graph.svg',
+				'link' => './estadisticas',
+				'img_source' => 'images/graph.svg',
 				'text' => 'Estadísticas',
 			]
 		],
+		-1 => [
+			[
+				'link' => './nuevos',
+				'img_source' => 'images/edit.svg',
+				'text' => 'Documentos nuevos',
+			],
+			[
+				'link' => './validados',
+				'img_source' => 'images/files.svg',
+				'text' => 'Documentos validados',
+			],
+			[
+				'link' => './estadisticas',
+				'img_source' => 'images/graph.svg',
+				'text' => 'Estadísticas',
+			]
+		]
 	];
 	return $navbar_options[$index]; //
 }
-
-?>
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -47,9 +47,6 @@
             </form>
         </div>
     @endauth
-
-
-
 </body>
 
 </html>
